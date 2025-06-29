65654e173f0e5e8b21f44921d5cc68fee5bfbe03
knock-huynh <huynhtransongngoc@gmail.com>
2025-06-29 13:13:59 +0700

    feat: add configurable asset directory and port via flags and env vars
    Allow users to specify the asset directory and port using command-line
    flags (--asset-dir, --port) or environment variables (ASSET_DIR, PORT),
    with sensible defaults and directory validation.

--- main.go
+++ main.go
@@ -1,24 +1,48 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
-)
-
-const (
-	filepathRoot = "./assets"
-	port         = "8080"
+	"os"
 )
 
 func main() {
+	defaultDir := "./assets"
+	defaultPort := "8080"
+	var assetDir, port string
+
+	flag.StringVar(&assetDir, "asset-dir", "", "directory to serve files from")
+	flag.StringVar(&port, "port", "", "port to serve on")
+	flag.Parse()
+
+	// Check flags, then env, then defaults
+	if assetDir == "" {
+		assetDir = os.Getenv("ASSET_DIR")
+		if assetDir == "" {
+			assetDir = defaultDir
+		}
+	}
+	if port == "" {
+		port = os.Getenv("PORT")
+		if port == "" {
+			port = defaultPort
+		}
+	}
+
+	// Validate asset directory exists
+	if info, err := os.Stat(assetDir); err != nil || !info.IsDir() {
+		log.Fatalf("Invalid asset directory: %s", assetDir)
+	}
+
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/", http.FileServer(http.Dir(assetDir)))
 
 	s := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
-	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Printf("Serving files from %s on port: %s\n", assetDir, port)
 	log.Fatal(s.ListenAndServe())
 }
