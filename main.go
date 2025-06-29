7328ac2c2fc756d5475b6d48b51e026cf47af54b
knock-huynh <huynhtransongngoc@gmail.com>
2025-06-29 17:56:01 +0700

    feat(health): add healthz handler

--- main.go
+++ main.go
@@ -13,6 +13,12 @@ const (
 	defaultPort = "8080"
 )
 
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	var assetDir, port string
 
@@ -38,7 +44,9 @@ func main() {
 	}
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(absAssetDir)))
+
+	mux.HandleFunc("/healthz", healthHandler)
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(absAssetDir))))
 
 	s := &http.Server{
 		Addr:    ":" + port,
